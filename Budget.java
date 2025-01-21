d37013f649b5ae49ac897298c547c6de3b7f8d14
Matthew Cruz <matthew.j.cruz111@gmail.com>
2025-01-21 17:59:23 -0600

    Small changes
    Adding net/gross yearly/weekly options for income.

--- Budget.java
+++ Budget.java
@@ -3,27 +3,34 @@ import java.util.ArrayList;
 
 
 public class Budget {
-
+	
+	static double payRate;
 	static double hours;
-	static double income;
+	static double income = 1;
 
 	static double tax;
-	static double monthlyExpenses;
-
-
-	static double payRate = 13;
 	static double taxRate = 0.0765;
-	//Make this category final constants.
+	static double monthlyExpenses;
+	//Make tax rate final constant.
 /*
-	ArrayList<double> expenseAmt = new ArrayList<double>();
-	ArrayList<String>  expenseName = new ArrayList<String>;
+	ArrayList<double> expAmt = new ArrayList<double>();
+	ArrayList<String>  expName = new ArrayList<String>;
+	
+	Figure out how to store collected data, and subsequently collect/extract from it.
+	
+	Start program listing current info and ask if any changes need to be made.
+	
+	Remind user of recurring charges.
 */
 
 
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
 
-		do{
+		while (income > 0)
+		{
+			System.out.println("Enter payrate.");
+			payRate = kb.nextDouble();
 			System.out.println("Enter hours worked.");
 			hours = kb.nextDouble();
 			income += payRate * hours;
@@ -35,18 +42,9 @@ public class Budget {
 			tax = income * taxRate;
 
 			//Calculate hours worked. If hours worked is over 40, then subtract 40 from hours worked snd multiply it by time&1/2.
-			System.out.println("Your gross income is: " + income);
-
-			System.out.println("Withholding tax, you're left with: " + (income - tax) + "\n");
-		}while (income > 0);
-
-
-
-	}
-}
-//run methods to collect and tally income and expense.
-
-//determine whether a quantity is weekly or monthly and calculate accordingly.
-
-//have the method procure a name and a quantity, put the results in a 2d array. to be extracted and tabulated. also list if monthly or weekly.
-
+			System.out.print("Your gross income is: ");
+			System.out.printf("%,.2f\n", income);
+			
+	System.out.println("Yearly, this is: ");
+	System.out.printf("%.2f\n", (income*52));
+	System.
