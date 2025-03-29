734bbe1bf078e4b53dc4c4275b8abd848bf5ccec
Matthew Cruz <matthew.j.cruz111@gmail.com>
2025-03-29 10:01:45 -0500

    Deletions
    Streamlining the project by removing extraneous files.

--- Budget.java
+++ Budget.java
@@ -3,51 +3,63 @@ import java.util.ArrayList;
 
 
 public class Budget {
- 
- static double payRate;
- static double hours;
- static double income = 1;
 
- static double tax;
- static double taxRate = 0.0765;
- static double monthlyExpenses;
- //Make tax rate final constant.
-/*
- ArrayList<double> expAmt = new ArrayList<double>();
- ArrayList<String>  expName = new ArrayList<String>;
- 
- Figure out how to store collected data, and subsequently collect/extract from it.
- 
- Start program listing current info and ask if any changes need to be made.
- 
- Remind user of recurring charges.
-*/
+	static double payRate;
+	static double hours;
+	static double income = 1;
+
+	static double tax;
+	static double taxRate = 0.0765; //Make tax rate final constant. Maybe. IDK.
+	static double rent;
+	static double utilities;
+	ArrayList<Double> expenseNum;
+	ArrayList<String> expenseName;
+
+	public static void getExpenses() {
+		String cont = "y";
+		Scanner exp = new Scanner(System.in);
+		while (cont.equals("y")||cont.equals("Y")){
+			System.out.print("Enter an expense, i.e. a bill or weekly grocery trip: ");
+			expenseNum.add(exp.nextDouble());
+			System.out.print("Enter a name for this expense: ");
+			expenseName.add(exp.nextLine());
+			System.out.print("Add another expense? y/n");
+		}
+	}
 
 
- public static void main(String[] args) {
-  Scanner kb = new Scanner(System.in);
 
-  while (income > 0)
-  {
-   System.out.println("Enter payrate.");
-   payRate = kb.nextDouble();
-   System.out.println("Enter hours worked.");
-   hours = kb.nextDouble();
-   income += payRate * hours;
-   if (hours > 40) {
-    income = ((hours - 40) * (payRate * 1.5) + (payRate * 40));
-   } else {
-    income = payRate * hours;
-   }
-   tax = income * taxRate;
+	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
 
-   //Calculate hours worked. If hours worked is over 40, then subtract 40 from hours worked snd multiply it by time&1/2.
-   System.out.print("Your gross weekly income is: ");
-   System.out.printf("%,.2f\n", income);
-   System.out.print("Yearly, this is: ");
-   System.out.printf("%,.2f\n", (income*52));
-   System.out.println("Your net weekly income is: ");
-   System.out.printf("")
-   System.out.print("Yearly, this is: ")  }
- }
+		while (income > 0) {
+//Data Collection			
+			System.out.print("Enter hourly payrate: ");
+			payRate = kb.nextDouble();
+			System.out.println("Enter hours worked: ");
+			hours = kb.nextDouble();
+			System.out.print("Enter a rent price:");
+			rent = kb.nextDouble();
+
+//Calculate hours worked. If hours worked is over 40, then subtract 40 from hours worked snd multiply it by time&1/2.
+			income += payRate * hours;
+			if (hours > 40) {
+				income = ((hours - 40) * (payRate * 1.5) + (payRate * 40));
+			} else {
+				income = payRate * hours;
+			}
+			tax = income * taxRate;
+
+			System.out.print("Your gross weekly income is: ");
+			System.out.printf("%,.2f\n", income);
+			System.out.print("Yearly, this is: ");
+			System.out.printf("%,.2f\n", (income * 52));
+			System.out.println("Your net weekly income is: ");
+			System.out.printf("%,.2f\n", (income - tax));
+			System.out.print("Yearly, this is: ");
+			System.out.printf("%,.2f\n", (income - tax) * 52);
+		}
+	}
 }
+//yearly income calculated in one loop, expenses another.
+//to begin, pedefined costs such as rent and ut
